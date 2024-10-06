7936bd17b09328a8a117635e07814212d2917ae5
annkurrrr <ankur.kunde@gmail.com>
2024-10-06 13:42:35 +0530

    added extra feature

---
+++ TagRacefinal.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    start:
+    srand(time(0));{
+        int Pposition=3,Cposition=0,Pchoice,Cchoice,difficulty,n,i;
+        printf("\nWELCOME TO TAG-RACE!!!!\n\n");
+        printf("RULES:\n");
+        printf("1.If player and computer stand at same position, player is out.\n");
+        printf("2.If the difference between positions of player and computer exceed a certain number(depending on difficulty), player is out.\n");
+        printf("3.Reach 100 before the computer to win.\n\n");
+        printf("SELECT DIFFICULTY(1-easy, 2-medium, 3-hard):");
+        scanf("%d",&difficulty);
+        printf("\n");
+        if(difficulty==1){
+            n=8;
+            printf("Difficulty:easy\nDifference between position of player and computer cannot exceed %d.\n\n",n);
+        }
+        else if(difficulty==2){
+            n=7;
+            printf("Difficulty:medium\nDifference between position of player and computer cannot exceed %d.\n\n",n);
+        }
+        else{
+            n=6;
+            printf("Difficulty:hard\nDifference between position of player and computer cannot exceed %d.\n\n",n);
+        }
+        printf("Player is standing at %d.\n",Pposition);
+        printf("Computer is standing at %d.\n\n",Cposition);
+        while(Pposition!=Cposition){
+            printf("===========================================\n\n");
+            printf("enter a number of your choice(from 0 to %d):",n);
+            scanf("%d",&Pchoice);
+            printf("\n");
+            if(Pchoice>n||Pchoice<0){
+                printf("play again\n");
+                continue;
+            }
+            printf("Player chose %d.\n",Pchoice);
+            Cchoice=rand()%(n+1);
+            printf("Computer chose %d.\n\n",Cchoice);
+            Pposition=Pposition+Pchoice;
+            Cposition=Cposition+Cchoice;
+            printf("Player is standing at %d.\n",Pposition);
+            printf("Computer is standing at %d.\n\n",Cposition);
+            if(Pposition==Cposition){
+                printf("Computer caught you lol. Try again!\n\n");
+                printf("Your score:%d\n\n",Pposition);
+                break;
+            }
+            else if(Pposition>=100&&Cposition<100||Pposition>=100&&Cposition<Pposition){
+                printf("You win!! That was sick!!\n");
+                printf("Your score:%d\n\n",Pposition);
+                break;
+            }
+            else if(Cposition>=100&&Pposition<100||Cposition>=100&&Pposition<Cposition){
+                printf("Computer beat you!! Try again!!\n");
+                printf("Your score:%d\n\n",Pposition);
+                break;
+            }
+            if(difficulty==1){
+                if(Pposition-Cposition>8||Cposition-Pposition>8){
+                    printf("You lose. Mind the gap lil bro.\n\n");
+                    printf("Your score:%d\n\n",Pposition);
+                    break;
+                }    
+            }
+            else if(difficulty==2){
+                if(Pposition-Cposition>7||Cposition-Pposition>7){
+                    printf("You lose. Mind the gap lil bro.\n\n");
+                    printf("Your score:%d\n\n",Pposition);
+                    break;
+                }    
+            }
+            else if(difficulty==3){
+                if(Pposition-Cposition>6||Cposition-Pposition>6){
+                    printf("You lose. Mind the gap lil bro.\n\n");
+                    printf("Your score:%d\n\n",Pposition);
+                    break;
+                }       
+            }
+        }
+        printf("Try again?(1-Yes, 0-No):");
+        scanf("%d",&i);
+        if(i==1){
+        goto start;
+        }
+        else{
+            return 0;
+        }
+    }
+}
